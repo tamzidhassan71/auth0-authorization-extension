a7773dd2781be908c948827907ef50368b32c55c
Tamzid Hasan Joy <121254129+tamzidhassan71@users.noreply.github.com>
2026-07-25 07:47:52 +0600

    Delete tools directory

--- tools/get_version.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console, import/no-dynamic-require */
-
-const path = process.argv[3] || '../package.json';
-const attributes = require(path);
-const value = attributes['version'];
-
-if (value !== undefined) {
-  console.log(value);
-}
